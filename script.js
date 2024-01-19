efdbb497d97dd6223b8ad8344f344ca43476eb4d
Max de Vocht <maxdevocht@hotmail.com>
2024-01-19 11:54:35 +0100

    Finished
    Adjusted width, and Splide elements

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 var splide = new Splide( '.splide', {
     perPage: 3,
-    gap    : '2rem',
+    perMove: 1,
+    gap    : '1.5rem',
+    padding: '3rem',
+    type: 'loop',
+    drag: 'free',
+    snap: true,
+    autoplay: true,
     breakpoints: {
       640: {
         perPage: 2,
-        perMove: 1,
         gap    : '.7rem',
-        type: 'loop',
-        drag: 'free',
-        snap: true,
-        autoplay: true,
+        padding: '1.5rem',
       },
       480: {
         perPage: 1,
         gap    : '.7rem',
-
+        padding: '1.5rem',
       },
     },
 } );
