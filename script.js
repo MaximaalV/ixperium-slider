1d901e8684b146705a7214ee45cd61f6a6c5451f
Max de Vocht <maxdevocht@hotmail.com>
2024-01-19 10:51:09 +0100

    Update
    Added images and adjusted the height in JS

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ var splide = new Splide( '.splide', {
       640: {
         perPage: 2,
         gap    : '.7rem',
-        height : '6rem',
+
       },
       480: {
         perPage: 1,
         gap    : '.7rem',
-        height : '6rem',
+
       },
     },
 } );
